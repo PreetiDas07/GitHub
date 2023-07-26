33853aa61f95503c8dfacacd5c6988f2a7b7e7dc
naidu684 <123893305+chinnamnaidu@users.noreply.github.com>
2023-07-26 17:47:27 +0530

    Add focus on input field by clicking on slash key

--- src/components/RepoContext.js
+++ src/components/RepoContext.js
@@ -5,7 +5,9 @@ export const RepoContext = createContext();
 const RepoProvider = ({ children }) => {
   const [searchTerm, setSearchTerm] = useState("");
   const [repositories, setRepositories] = useState([]);
-  const repoSearchData = repositories[0];;
+  const repoSearchData = repositories[0];
+
+  
 
   return (
     <RepoContext.Provider value={{ searchTerm, setSearchTerm, repositories, setRepositories, repoSearchData }}>
--- src/screens/RepoSearch.js
+++ src/screens/RepoSearch.js
@@ -1,18 +1,33 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef, useEffect } from 'react';
 import { RepoContext } from '../components/RepoContext';
 import axios from 'axios';
 
 export default function RepoSearch() {
 
-    const { searchTerm, setSearchTerm, setRepositories} = useContext(RepoContext);
-
+    const { searchTerm, setSearchTerm, setRepositories } = useContext(RepoContext);
+    const inputRef = useRef(null);
+    
     const handleChange = (event) => {
         setSearchTerm(event.target.value);
     };
+    const handleSlashClick = () => {
+        inputRef.current.focus();
+    };
+
+    const handleKeyDown = (e) => {
+        if (e.key === '/') {
+            e.preventDefault();
+            inputRef.current.focus();
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('keydown', handleKeyDown);
+    }, []);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        if(!searchTerm) {
+        if (!searchTerm) {
             alert("Please enter a repositories")
             return;
         }
@@ -20,8 +35,8 @@ export default function RepoSearch() {
             const response = await axios.get(`https://api.github.com/search/repositories?q=${searchTerm}`);
             const repoData = response.data.items;
             const matchedRepository = repoData.find(repo => (repo.full_name === searchTerm.trim("")));
-    
-             if (!matchedRepository) {
+
+            if (!matchedRepository) {
                 alert("Some repositories have names that do not match the search term.");
             } else {
                 setRepositories(repoData);
@@ -35,11 +50,14 @@ export default function RepoSearch() {
         <div>
             <form onSubmit={handleSubmit}>
                 <input className='repoSearchInput'
+                    ref={inputRef}
                     type='text'
                     placeholder='Search or jump to…'
                     value={searchTerm}
                     onChange={handleChange}
+                    onKeyDown={handleKeyDown}
                 />
+                <span className='slash' onClick={handleSlashClick}>/</span>
             </form>
         </div>
     );
