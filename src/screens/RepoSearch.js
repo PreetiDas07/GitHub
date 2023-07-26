231ea3b65da10eb075034af1cb452f709e323371
naidu684 <123893305+chinnamnaidu@users.noreply.github.com>
2023-07-26 14:45:54 +0530

    Add edge cases to repoSearch and delete duplicate file of repoSearch

--- src/screens/RepoSearch.js
+++ src/screens/RepoSearch.js
@@ -1,12 +1,10 @@
-// RepoSearch.js
 import React, { useContext } from 'react';
 import { RepoContext } from '../components/RepoContext';
 import axios from 'axios';
 
-
 export default function RepoSearch() {
 
-    const { searchTerm, setSearchTerm, setRepositories } = useContext(RepoContext);
+    const { searchTerm, setSearchTerm, setRepositories} = useContext(RepoContext);
 
     const handleChange = (event) => {
         setSearchTerm(event.target.value);
@@ -14,14 +12,25 @@ export default function RepoSearch() {
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+        if(!searchTerm) {
+            alert("Please enter a repositories")
+            return;
+        }
         try {
             const response = await axios.get(`https://api.github.com/search/repositories?q=${searchTerm}`);
-            setRepositories(response.data.items);
+            const repoData = response.data.items;
+            const matchedRepository = repoData.find(repo => (repo.full_name === searchTerm.trim("")));
+    
+             if (!matchedRepository) {
+                alert("Some repositories have names that do not match the search term.");
+            } else {
+                setRepositories(repoData);
+            }
         } catch (error) {
             console.error("Error fetching repositories", error);
+            alert("An error occurred while fetching repositories. Please try again later.");
         }
     };
-
     return (
         <div>
             <form onSubmit={handleSubmit}>
@@ -30,9 +39,7 @@ export default function RepoSearch() {
                     placeholder='Search or jump to…'
                     value={searchTerm}
                     onChange={handleChange}
-
                 />
-
             </form>
         </div>
     );
