63dbe6ff5025c0fb66d2df71619e070dfa368f27
naidu684 <123893305+chinnamnaidu@users.noreply.github.com>
2023-07-27 17:30:43 +0530

    Delete unnecessary files and consoles

--- src/components/RepoContext.js
+++ src/components/RepoContext.js
@@ -6,8 +6,6 @@ const RepoProvider = ({ children }) => {
     const [searchTerm, setSearchTerm] = useState("");
   const [repositories, setRepositories] = useState([]);
   const repoSearchData = repositories[0];
-console.log("repo" , repositories);
-console.log("search", repoSearchData);
 
   return (
     <RepoContext.Provider value={{searchTerm, setSearchTerm,  repositories, setRepositories, repoSearchData}}>
